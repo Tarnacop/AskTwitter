ef369a6ce2ee6025d8ac2f84490407ff394a7379
Andrei-Marius Longhin <axl518@cs.bham.ac.uk>
2016-10-01 13:22:59 +0100

    Add basic server functionality

--- server/server.js
+++ server/server.js
@@ -1,3 +1,12 @@
 /**
  * Created by aml on 01/10/2016.
  */
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+});
+
+app.listen(5000);
